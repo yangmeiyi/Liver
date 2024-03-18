bb8fa2d24d4afc79a6c1371458efe56d33b64147
MeiMei <50604363+yangmeiyi@users.noreply.github.com>
2024-03-18 17:09:32 +0800

    Update variables.py

--- Classification/utils/variables.py
+++ Classification/utils/variables.py
@@ -2,13 +2,6 @@ up_load = True
 pretrained = True
 Need_train = True
 save_model = True
-dis = False
-
-
-MIXUP = False
-Smooth = False
-Seesaw = False
-cosin = False
 image_size = 224
 padding_size = 0
 crop_size = 224
@@ -17,11 +10,8 @@ batchsize_test = 256
 batchsize_val = 256
 gpus = "1,2,3,4,5,6,7"
 epochs = 300
-beta = 0.
-cutmix_prob = 0.
-
 ct = False
 ap = False
 pvp = False
-ap_pvp = False
-ct_ap_pvp = True
+ap_pvp = True
+ct_ap_pvp = False
