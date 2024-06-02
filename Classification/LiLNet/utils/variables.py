d2f949bb456106c2df0ba43cddf36286ec58a211
Meiyi Yang <50604363+yangmeiyi@users.noreply.github.com>
2024-06-02 19:55:50 +0800

    Add files via upload

---
+++ Classification/LiLNet/utils/variables.py
@@ -0,0 +1,12 @@
+up_load = True
+pretrained = False
+Need_train = True
+save_model = True
+image_size = 224
+padding_size = 0
+crop_size = 224
+ct = False
+ap = False
+pvp = False
+ap_pvp = True
+ct_ap_pvp = False
